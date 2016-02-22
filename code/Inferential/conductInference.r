f86501fa2c9c2929a63e225855913261c5ca95ee
ikuznet1 <ivan.kuznetsov1@gmail.com>
2016-02-21 23:45:37 -0500

    Code

--- code/Inferential/conductInference.r
+++ code/Inferential/conductInference.r
@@ -1,6 +1,8 @@
 #Driver funciton for marginal_inference. Feeds individual columns from input data to marginal_inference to conduct hypothesis tests.
 
 conductInference <- function(data,maxK = 3, nboot = 10, power = TRUE, columns_to_analyze = 4 * c(0:23) + 1){
+	require(ggplot2)
+	require(reshape)
 	
 	plots <- list()
 	tests_to_run <- c()
@@ -9,21 +11,23 @@ conductInference <- function(data,maxK = 3, nboot = 10, power = TRUE, columns_to
 	}
 	
 	for (i in columns_to_analyze){
-		test_result <- capture.output(marginal_inference(data[,i],maxK = maxK, nboot = nboot, power = power),file = "NUL")
+		capture.output(test_result <- marginal_inference(data[,i],maxK = maxK, nboot = nboot, power = power),file = "NUL")
 		pvals <- rep(-1,maxK-1)
 		test <- test_result[[1]]
 		power_curve <- test_result[[2]]
 		p1 <- list()
+		colnames(power_curve)[2:length(colnames(power_curve))] <- paste("c",c(2:length(power_curve)),sep="")
 		for (j in c(1:(maxK - 1))){
-			p1[[j]] <- ggplot(power_curve,aes(x = Samples)) + geom_point(aes_string(paste(i,"versus",i + 1,"H_a",sep=""))) + geom_point(aes_string(paste(i,"versus",i + 1,"H_0",sep=""))) + guides(fill=guide_legend(title="Distribution")) + xlab("Samples") + ylab("Power") + ggtitle(paste("Power for test of k =",j,"versus k =",j + 1))
-			print(p[[j]])
-			pvals[j] <- test[[j]]$pval
-			
+			df <- power_curve[,c(1,2 * j, 2 * j + 1)]
+			df <- melt(df,"Samples")
+			p1[[j]] <- ggplot(df,aes_string(x = "Samples", y = "value", color = "variable")) + geom_point() + xlab("Samples") + ylab("Power") + ggtitle(paste("Power for test of k =",j,"versus k =",j + 1)) + scale_colour_manual("Distribution",values = c("blue","red"),labels=c("Alt", "Null"))
+			print(p1[[j]])
+			pvals[j] <- test[[j]]$pval		
 		}	
-		pval_df <- cbind(factor(tests_to_run),pvals)
-		p2 <- ggplot(pval_df,aes(x = tests_to_run, y = pval))
-		
-		plots <- list(plots, p1)
+		pval_df <- as.data.frame(cbind(tests_to_run,pvals))
+		p2 <- ggplot(pval_df,aes(x = tests_to_run, y = pvals)) + geom_point() + xlab("Test") + ylab("P-value") + ggtitle("Inference Testing Results")
+		print(p2)
+		plots <- list(plots, p1,p2)
 		
 	}
 	
