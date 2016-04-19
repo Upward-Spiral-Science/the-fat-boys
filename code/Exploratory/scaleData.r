af5490dfe2595ebd5fb89427830c472caca8ab2b
ikuznet1 <ivan.kuznetsov1@gmail.com>
2016-04-19 19:15:56 -0400

    Update

--- code/Exploratory/scaleData.r
+++ code/Exploratory/scaleData.r
@@ -1,6 +1,7 @@
-#Scales each column of the input data frame by converting to Z scores
+#Scales each column of the input data frame by converting to Z scores and optionally log scaling the data
 
-scaleData <- function(data,center = TRUE,scale = TRUE){
+scaleData <- function(data,center = TRUE,scale = TRUE, log = TRUE){
 	data_scaled <- scale(data, center = center,scale = scale)
+	data_scaled <- log(data_scaled + 1) / log(10)
 	return(data_scaled)
 }
